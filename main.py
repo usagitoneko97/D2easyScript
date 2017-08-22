4c30ee3c1ca8f3fcbfe0f013f706a671354429cd
Ho Guo Xian <hogouxian@gmail.com>
2017-08-22 22:49:22 +0800

    added and tested alacrity and poof

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
-from pynput.keyboard import Key, Controller
-from pynput import keyboard
+import pythoncom
+import pyHook
+from sendKeysTest import PressKey, ReleaseKey
 import pyautogui
-keyboard1 = Controller()
+import time
 
-def on_press(key):
-    try:
-        if key.char == '+':
-            pyautogui.moveTo(1120, 968)
-            pyautogui.dragTo(1112, 929, 0.1001, button='left')
-        elif key.char == '-':
-            keyboard1.press(Key.space)
-            keyboard1.release(Key.space)
+Q = 0x10
+W = 0x11
+E = 0x12
+R = 0x13
+D = 0x20
+F = 0x21
+TAB = 0x0F
 
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+def OnKeyboardEvent(event):
+    print(event.Key)
+    if event.Key == 'Subtract':
+        print("here")
+        print("event key: ", event.Key)
+        # alacrity()
+        poof()
+    elif event.Key == 'Add':
+        pyautogui.moveTo(1125, 968)
+        pyautogui.dragTo(1125, 929, 0.1001, button='left')
+    # return True to pass the event to other handlers
+    return True
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
 
+def alacrity():
+    pressKeyboard(W)
+    pressKeyboard(W)
+    pressKeyboard(E)
+    pressKeyboard(R)
+    time.sleep(0.2)
+    pressKeyboard(D)
+    pressKeyboard(D)
+    time.sleep(0.2)
+    pressKeyboard(W)
+    pressKeyboard(W)
+    pressKeyboard(W)
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+def poof():
+    pressKeyboard(TAB)
+    pressKeyboard(W)
+    pressKeyboard(TAB)
+    pressKeyboard(W)
+    pressKeyboard(TAB)
+    pressKeyboard(W)
+
+def earthSpiritStun():
+    pressKeyboard(D)
+    pressKeyboard(D)
+    PressKey(Q)
+
+def pressKeyboard(vkCode):
+    PressKey(vkCode)
+    ReleaseKey(vkCode)
+
+# create a hook manager
+hm = pyHook.HookManager()
+# watch for all mouse event
+hm.KeyDown = OnKeyboardEvent
+# set the hook
+hm.HookKeyboard()
+# wait forever
+pythoncom.PumpMessages()
