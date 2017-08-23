7a6408e7a0548250aa33f19cc532d3ef8b2c2005
Ho Guo Xian <hogouxian@gmail.com>
2017-08-23 23:23:26 +0800

    added get max and min value of x in get_hero_loc

--- main.py
+++ main.py
@@ -39,24 +39,28 @@ def OnKeyboardEvent(event):
 
         x, y = get_hero_loc()
 
-        pyautogui.moveTo(x, y+40)
-        pressKeyboard(D)
-        pyautogui.click()
-        # cv2.imshow('window', image)
-    elif event.Key == 'Divide':
-        pressKeyboard(C)
+        pyautogui.moveTo(x, y+40)                        # Note ************************
+        pressKeyboard(D)                                 # This is some useless program
+        pyautogui.click()                                # develop this just to play games
+        # cv2.imshow('window', image)                    # Please make a rocket instead
+    elif event.Key == 'Divide':                          # I just want to type something
+        pressKeyboard(C)                                 # Your keyboard really nice to type
         x, y = get_hero_loc()
         pyautogui.moveTo(x, y+40)
         pyautogui.click()
         time.sleep(0.85)
         pressKeyboard(D)
         pyautogui.click()
-    elif event.Key == 'G':
+    # elif event.Key == 'G':
         pressKeyboard(C)
         pyautogui.click()
         time.sleep(1.5)
         pressKeyboard(W)
         pyautogui.click()
+    elif event.Key == 'Numpad3':
+        x, y = get_hero_loc()
+        # print(x, y)
+
     # return True to pass the event to other handlers
     return True
 
@@ -67,15 +71,33 @@ def get_hero_loc():
     upper = np.array([200, 60, 0])
     shape_mask = cv2.inRange(printscreen, lower, upper)
     image, contours, hierarchy = cv2.findContours(shape_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # for i in contours:
+    #     np.sort(i)
+    previous_max = 0
+    previous_min = 1080
+    for element in contours:
+        max_val = np.amax(element)
+        min_val = np.amin(element)
+        if max_val > previous_max:
+            previous_max = max_val
+        if min_val < previous_min:
+            previous_min = min_val
+
+    print(previous_max)
+    print(previous_min)
+    print("------------------")
+    print(contours)
+
     # print(type(contours))
+    # print((contours[0]))
     randPlace = contours[0][0]
     # print(randPlace.flat[0])
     # print(randPlace.flat[1])
     return randPlace.flat[0], randPlace.flat[1]
 
 def processImg(image):
+    # convert to gray
     original_image = image
-    #convert to gray
     processedImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     processedImg = cv2.Canny(processedImg, threshold1=200, threshold2=300)
     return processedImg
