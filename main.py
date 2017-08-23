6b40e6424ab50d11615def10f28561d036f84589
Ho Guo Xian <hogouxian@gmail.com>
2017-08-23 18:04:45 +0800

    identify hero location, not nearest

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ E = 0x12
 R = 0x13
 D = 0x20
 F = 0x21
+C = 0x2E
 TAB = 0x0F
 
 def OnKeyboardEvent(event):
@@ -26,11 +27,58 @@ def OnKeyboardEvent(event):
         pyautogui.moveTo(1125, 968)
         pyautogui.dragTo(1125, 929, 0.1001, button='left')
     elif event.Key == 'Multiply':
-        printscreen = np.array(ImageGrab.grab())
-        cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        #demonstration
+        alacrity()
+        pressKeyboard(Q)
+        pressKeyboard(Q)
+        pressKeyboard(Q)
+        pressKeyboard(R)
+        time.sleep(0.1)
+
+        # TODO: get to the center of hero with certain algorithm
+
+        x, y = get_hero_loc()
+
+        pyautogui.moveTo(x, y+40)
+        pressKeyboard(D)
+        pyautogui.click()
+        # cv2.imshow('window', image)
+    elif event.Key == 'Divide':
+        pressKeyboard(C)
+        x, y = get_hero_loc()
+        pyautogui.moveTo(x, y+40)
+        pyautogui.click()
+        time.sleep(0.85)
+        pressKeyboard(D)
+        pyautogui.click()
+    elif event.Key == 'G':
+        pressKeyboard(C)
+        pyautogui.click()
+        time.sleep(1.5)
+        pressKeyboard(W)
+        pyautogui.click()
     # return True to pass the event to other handlers
     return True
 
+def get_hero_loc():
+    # get the screen, filter out every color except red, and find contours
+    printscreen = np.array(ImageGrab.grab())
+    lower = np.array([170, 40, 0])
+    upper = np.array([200, 60, 0])
+    shape_mask = cv2.inRange(printscreen, lower, upper)
+    image, contours, hierarchy = cv2.findContours(shape_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # print(type(contours))
+    randPlace = contours[0][0]
+    # print(randPlace.flat[0])
+    # print(randPlace.flat[1])
+    return randPlace.flat[0], randPlace.flat[1]
+
+def processImg(image):
+    original_image = image
+    #convert to gray
+    processedImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processedImg = cv2.Canny(processedImg, threshold1=200, threshold2=300)
+    return processedImg
 
 def alacrity():
     pressKeyboard(W)
